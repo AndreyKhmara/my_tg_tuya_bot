01db40e4948706488b897bd552ecae4d0cfc56c8
AndreyKhmara <kelevra101smu@gmail.com>
2025-09-30 18:22:03 +0300

    tas_03: add deploy test bot

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import os
 from telebot import TeleBot
 from tuya_connector import TuyaOpenAPI
 from handlers import (start, devices, controlHandlers)
 from state import user_selected_device
+# t.me/MySmartTuyaTestBot - адрес бота
 # команда для запуска при локальной разработке - python main.py
-from config import TOKEN, API_ENDPOINT, ACCESS_ID, ACCESS_KEY, DEVICE_LIST
+from config import API_ENDPOINT, ACCESS_ID, ACCESS_KEY, DEVICE_LIST
 
+TOKEN = os.getenv("TEST_BOT_TOKEN")
 bot = TeleBot(TOKEN)
 
 openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
